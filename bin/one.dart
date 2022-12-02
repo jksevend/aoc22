132f3e74ad334ba0c54ee10b8382563e2dc0529c
Julian <julxott@outlook.de>
2022-12-02 15:57:48 +0100

    Initial commit

---
+++ bin/one.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> args) async {
+  // Read file content and split
+  final String fileContent = await _readElvs();
+  final List<String> fileLines = fileContent.split('\r\n');
+
+  final List<List<String>> parsedContent = [];
+  final List<String> elvContent = [];
+
+  for (var i = 0; i < fileLines.length; i++) {
+    if (fileLines[i] == '') {
+      parsedContent.add(List.from(elvContent));
+      elvContent.clear();
+    } else {
+      elvContent.add(fileLines[i]);
+    }
+  }
+  // Map every string entry to an elv class
+  final List<Elv> elvs = parsedContent
+      .map(
+          (elvList) => Elv(food: elvList.map((caloriesValue) => int.parse(caloriesValue)).toList()))
+      .toList();
+
+  final List<int> sums = elvs.map((elv) => elv.sum).toList();
+  print(sums.reduce(max));
+}
+
+class Elv {
+  final List<int> food;
+
+  Elv({required this.food});
+
+  int get sum {
+    int sum = 0;
+    for (var element in food) {
+      sum += element;
+    }
+    return sum;
+  }
+}
+
+Future<String> _readElvs() async {
+  final File file = File('assets/elvs.txt');
+  return await file.readAsString();
+}
