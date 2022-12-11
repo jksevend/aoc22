6a4b859c2bd6b3da7b3283ea7c166e82025a29d5
jksevend <julxott@outlook.de>
2022-12-11 10:58:39 +0100

    feat: Add Part1 for 2022-12-06

---
+++ bin/6_tuning_trouble.dart
@@ -0,0 +1,23 @@
+import 'utility/file_util.dart';
+
+const int markerLength = 4;
+
+void main() async {
+  final String fileContent =
+      await readFileContent('assets/6_tuning_trouble.txt');
+
+  final List<String> fileChars = fileContent.split('');
+  int processed = 0;
+
+  for (int i = 0; i < fileChars.length; i++) {
+    final List<String> currentMarker = fileChars.sublist(i, i + markerLength);
+    final List<String> uniqueMarker = currentMarker.toSet().toList();
+
+    if (uniqueMarker.length == markerLength) {
+      processed = i + markerLength;
+      break;
+    }
+  }
+
+  print(processed);
+}
